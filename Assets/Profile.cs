6b04ac801f438a851c9eea0ca9577e634bb101f5
praffulUtl <praffulbharti.utlsolution@gmail.com>
2024-06-28 10:20:51 +0530

    game sync (in progress)

--- Assets/Profile.cs
+++ Assets/Profile.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,7 +10,7 @@ public class Profile : MonoBehaviour
     [SerializeField] TextMeshProUGUI nameTxt;
     [SerializeField] RawImage image;
     [Header("Edit section")]
-    [SerializeField] TMP_InputFieldEditor nameInput;
+    [SerializeField] TMP_InputField nameInput;
     [SerializeField] Button pickImageBt;
 
     private void Start()
